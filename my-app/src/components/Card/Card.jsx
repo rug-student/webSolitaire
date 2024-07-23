ab69e2baa8717d7f343d623cbfb11cfc97867e2b
Leo Willmann <l.a.m.willmann@student.rug.nl>
2024-07-23 18:16:15 +0200

    Gave project some initial structure

---
+++ my-app/src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+
+  return (
+    <div className='CardStyle'>
+        <p>carp</p>
+    </div>
+  );
+}
+
+export default Card;
